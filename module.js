e0cc49f6df14ab64861a322f3a6c641ea7bddf2f
Seamus <sja4201@gmail.com>
2021-01-03 19:51:22 -0800

    Have I done a bad?
    Switched to release version, messed with things I don't understand. Lets do some more of that!

---
+++ module.js
@@ -0,0 +1,31 @@
+
+Hooks.once("ready", () => {
+
+    console.log("li-player-request | Registering socket callback");
+    /** @ts-ignore */
+    game.socket.on("module.li-player-request", function (data) {
+
+        const combat = game.combats.find((c) => c.id === data.combat);
+        combat.activateCombatant(data.combatant)
+    });
+});
+/** This hook runs whenever a player that doesn't have permission to modify the
+ * combat tracker clicks the activation button of a unit that still has
+ * activations left. The hook passes the combat and the combatant id.
+ */
+Hooks.on("LancerCombatRequestActivate", (combat, combatantId) => {
+    // Only request for owned combatants that have activations available
+    const combatant = combat.getCombatant(combatantId);
+    if (combatant.permission < 3 || combatant.flags.activations.value < 1 || !combat.started) {
+        return;
+    }
+
+    console.log(combatantId + "has activated initiative");
+
+    game.socket.emit("module.li-player-request", {
+        combat: combat.id,
+        combatant: combatantId,
+    });
+});
+export {};
+//# sourceMappingURL=module.js.map
